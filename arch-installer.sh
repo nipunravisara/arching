d9883bb05d8e80f4c4cc791ff90fe76b15fbd099
Nipun Ravisara <ravisara.dev@gmail.com>
2021-11-17 21:08:20 +0530

    Adding color codes.

--- arch-installer.sh
+++ arch-installer.sh
@@ -1,27 +1,33 @@
-echo "Starting installation..."
+# colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 2`
+reset=`tput sgr0`
+
+echo "${green}Starting installation...${reset}"
 
 # setup keyboard layout
-echo "-- Setting up keyboard layout."
+echo "${green}-- Setting up keyboard layout.${reset}"
 loadkeys us
 
 timedatectl set-ntp true
 
 # format EFI partitions
-echo "-- Formatting partitions."
+echo "${green}-- Formatting partitions.${reset}"
 lsblk
 
 # format efi partition.
-echo "Enter linux EFI partition: "
+echo "${yellow}Enter linux EFI partition: ${reset}"
 read linuxefipartition
 mkfs.fat -F 32 $linuxefipartition
 
 # format root partition.
-echo "Enter root partition: "
+echo "${yellow}Enter root partition: ${reset}"
 read rootpartition
 mkfs.ext4 $rootpartition
 
 # mounting partitons
-echo "-- Mounting partitons."
+echo "${green}-- Mounting partitons.${reset}"
 mkdir /mnt/boot
 mount $rootpartition /mnt
 mount $linuxefipartition /mnt/boot
@@ -29,11 +35,11 @@ mount $linuxefipartition /mnt/boot
 # -------------------------------------------------------------------------------------------------------------
 
 # install linux system and essentials.
-echo "-- Install linux system and essentials."
+echo "${green}-- Install linux system and essentials.${reset}"
 basestrap /mnt base base-devel runit elogind-runit linux-lts linux-firmware neovim curl git dosfstools
 
 # generate fstab
-echo "-- Generate fstab."
+echo "${green}-- Generate fstab.${reset}"
 fstabgen -U /mnt >> /mnt/etc/fstab
 
 
@@ -44,46 +50,46 @@ chmod +x /mnt/installer.sh
 #new-system-config
 
 # go to system
-echo "-- Move to new system."
+echo "${green}-- Move to new system.${reset}"
 artix-chroot /mnt ./installer.sh
 exit
 
 # set local time zone
-echo "-- Set time zone."
+echo "${green}-- Set time zone.${reset}"
 ln -sf /usr/share/zoneinfo/Asia/Colombo /etc/localtime
 
 # update hardware clock
-echo "-- Update hardware clock."
+echo "${green}-- Update hardware clock.${reset}"
 hwclock --systohc
 
 # set system language
-echo "-- Set system language."
+echo "${green}-- Set system language.${reset}"
 sed '/en_US.UTF-8 UTF-8/s/^#//' -i /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
 
 # crate hostname
-echo "-- Set system hostname."
-echo "Pick a hostname: "
+echo "${green}-- Set system hostname.${reset}"
+echo "${yellow}Pick a hostname: ${reset}"
 read hostname
 echo $hostname >> /etc/hostname
 
 # set hosts
-echo "-- Setup system hostfile."
+echo "${green}-- Setup system hostfile.${reset}"
 echo -e "\n127.0.0.1    localhost\n::1          localhost\n127.0.0.1    desktop.localdomain $hostname" >> /etc/hosts
 
 # install bootloader and relevent packages
-echo "-- Install bootloader and relevent packages."
+echo "${green}-- Install bootloader and relevent packages.${reset}"
 pacman --noconfirm -S grub os-prober ntfs-3g
 
 # choosing dual boot or single boot
 read -p "Do you wish to dualboot? [y/n]" answer
 if [[ $answer = y ]] ; then
      # mounting windows efi
-     echo "-- Mounting windows EFI."
+     echo "${green}-- Mounting windows EFI.${reset}"
      mkdir /boot/efi
      lsblk
-     echo "Enter windows EFI partition: "
+     echo "${yellow}Enter windows EFI partition: ${reset}"
      read winefipartition
      mount $winefipartition /boot/efi
 fi
@@ -99,11 +105,11 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # set root password
-echo "-- Set root user password."
+echo "${green}-- Set root user password.${reset}"
 passwd
 
 # install system packages
-echo "-- Install system packages."
+echo "${green}-- Install system packages.${reset}"
 pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop xcompmgr \
      mpv zathura zathura-pdf-mupdf firefox \
      xclip zip unzip unrar p7zip xdotool \
@@ -111,20 +117,20 @@ pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xback
      networkmanager networkmanager-runit
 
 # starting networkmanager.
-echo "-- Starting network manager"
+echo "${green}-- Starting network manager${reset}"
 ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default/
 
 #  setting up sudoers file.
-echo "-- Setting up sudoers file."
+echo "${green}-- Setting up sudoers file.${reset}"
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # ------------------------------------------------------------------------------------------------------------------
 
 # creating new user.
-echo "-- Creating new user."
-echo "Enter Username: "
+echo "${green}-- Creating new user.${reset}"
+echo "${yellow}Enter Username: ${reset}"
 read username
-echo "Enter Password: "
+echo "${yellow}Enter Password: ${reset}"
 read password
 passwd $password
 useradd -m -G wheel -s /bin/zsh $username
