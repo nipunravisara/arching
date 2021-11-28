fe4efc966f9e99d38c716d7b7400735c56ae48f5
Nipun Ravisara <34549795+nipunravisara@users.noreply.github.com>
2021-11-28 10:37:08 +0530

    Update arch-installer.sh
    package updates

--- arch-installer.sh
+++ arch-installer.sh
@@ -123,9 +123,8 @@ pacman-key --populate archlinux
 # install packages
 echo "${green}-- Install package${reset}"
 pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop \
-	xclip zip unzip unrar p7zip zsh rsync rofi networkmanager networkmanager-runit \
+	xclip zip unzip unrar p7zip zsh rsync rofi udisks2 ueberzug networkmanager networkmanager-runit \
 	bspwm sxhkd pamixer ranger sxiv mpv zathura zathura-pdf-mupdf firefox libnotify dunst alacritty
-
 
 # starting networkmanager.
 echo "${green}-- Starting network manager${reset}"
