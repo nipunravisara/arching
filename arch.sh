b0178f8dc12558bf6a5bea1245b06435f41f9499
Nipun Ravisara <34549795+nipunravisara@users.noreply.github.com>
2021-12-26 23:01:38 +0530

    WIP

--- arch.sh
+++ arch.sh
@@ -20,11 +20,6 @@ ROOT_SIZE=
 TIME_ZONE="Asia/Colombo"
 LOCALE="en_US.UTF-8"
 
-BASE_SYSTEM_PACKAGES=( base base-devel linux linux-headers linux-firmware archlinux-keyring )
-CONFIG_PACKAGES=(  )
-UTIL_PACKAGEs=( git )
-DEV_PACKAGEs=( git )
-
 # sort mirror list
 echo "${green}-- Pick best mirrors.${reset}"
 reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist --protocol https --download-timeout 5
@@ -56,7 +51,7 @@ echo && echo "Partitioning completed.  Press any key to continue..."; read empty
 
 # install linux system and essentials.
 echo "${green}-- Installing linux system and essentials.${reset}"
-pacstrap /mnt base base-devel linux linux-headers linux-firmware archlinux-keyring
+pacstrap /mnt base linux linux-headers linux-firmware base-devel archlinux-keyring
 
 # generate fstab
 echo "${green}-- Generating fstab.${reset}"
@@ -156,12 +151,12 @@ do
     case $opt in
         "Herbstluftwm")
             echo "${green}-- Installing $opt.${reset}"
-            arch-chroot /mnt su - "$USERNAME" -c pacman -S --noconfirm herbstluftwm
+            arch-chroot -u "$USERNAME" /mnt pacman -S --noconfirm herbstluftwm
             break
             ;;
         "BSPWM")
             echo "${green}-- Installing $opt.${reset}"
-            arch-chroot /mnt su - "$USERNAME" -c pacman -S --noconfirm bspwm
+             arch-chroot -u "$USERNAME" /mnt pacman -S --noconfirm bspwm
             break
             ;;
         "Skip, Install manually")
@@ -176,43 +171,43 @@ echo && echo "Window manager installed. Type any key to continue."; read empty
 # create folders
 cd $HOME
 echo "${green}-- Creating folders.${reset}"
-arch-chroot /mnt su - "$USERNAME" -c mkdir -p ~/Documents ~/Developments ~/Pictures/Wallpapers ~/Videos
+arch-chroot -u "$USERNAME" /mnt mkdir -p ~/Documents ~/Developments ~/Pictures/Wallpapers ~/Videos
 echo && echo "Folders created. Type any key to continue."; read empty
 
 # download wallpaper
 echo "${green}-- Downloading wallpaper.${reset}"
-arch-chroot /mnt su - "$USERNAME" -c curl -o ~/Pictures/Wallpapers/Green-leaves.jpeg https://images.pexels.com/photos/1072179/pexels-photo-1072179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260
+arch-chroot -u "$USERNAME" /mnt curl -o ~/Pictures/Wallpapers/Green-leaves.jpeg https://images.pexels.com/photos/1072179/pexels-photo-1072179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260
 echo && echo "Wallpaper downloaded. Type any key to continue."; read empty
 
 # install dotfiles
 cd $HOME
 echo "${green}-- Installing dotfiles.${reset}"
-arch-chroot /mnt su - "$USERNAME" -c git clone --bare https://github.com/nipunravisara/dots.git $HOME/.dotfiles
-arch-chroot /mnt su - "$USERNAME" -c echo ".dotfiles" >> .gitignore
-arch-chroot /mnt su - "$USERNAME" -c alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-arch-chroot /mnt su - "$USERNAME" -c dots config --local status.showUntrackedFiles no
-arch-chroot /mnt su - "$USERNAME" -c dots checkout
+arch-chroot -u "$USERNAME" /mnt git clone --bare https://github.com/nipunravisara/dots.git $HOME/.dotfiles
+arch-chroot -u "$USERNAME" /mnt echo ".dotfiles" >> .gitignore
+arch-chroot -u "$USERNAME" /mnt alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+arch-chroot -u "$USERNAME" /mnt dots config --local status.showUntrackedFiles no
+arch-chroot -u "$USERNAME" /mnt dots checkout
 
 # install oh-my-zsh and chnaging shell to zsh
 echo "${green}-- Changing shell to zsh.${reset}"
-arch-chroot /mnt su - "$USERNAME" -c sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+arch-chroot -u "$USERNAME" /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # install oh-my-zsh extentions
 echo "${green}-- Install oh-my-zsh extentions.${reset}"
-arch-chroot /mnt su - "$USERNAME" -c git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-arch-chroot /mnt su - "$USERNAME" -c git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+arch-chroot -u "$USERNAME" /mnt git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+arch-chroot -u "$USERNAME" /mnt git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # install ranger icons
 echo "${green}-- Install ranger icons.${reset}"
-arch-chroot /mnt su - "$USERNAME" -c git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+arch-chroot -u "$USERNAME" /mnt git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
 # remove unwated files
 echo "${green}-- Cleaning and linking.${reset}"
-arch-chroot /mnt su - "$USERNAME" -c rm -rf ~/.zshrc ~/.zsh_history ~/.bash_logout ~/.bash_profile ~/.bashrc ~/.shell.pre-oh-my-zsh ~/.zcompdump*
-arch-chroot /mnt su - "$USERNAME" -c ln -s ~/.config/x11/xinitrc .xinitrc
-arch-chroot /mnt su - "$USERNAME" -c ln -s ~/.config/x11/Xresources .Xresources
-arch-chroot /mnt su - "$USERNAME" -c ln -s ~/.config/zsh/zprofile .zprofile
-arch-chroot /mnt su - "$USERNAME" -c ln -s ~/.config/zsh/zshrc .zshrc
+arch-chroot -u "$USERNAME" /mnt rm -rf ~/.zshrc ~/.zsh_history ~/.bash_logout ~/.bash_profile ~/.bashrc ~/.shell.pre-oh-my-zsh ~/.zcompdump*
+arch-chroot -u "$USERNAME" /mnt ln -s ~/.config/x11/xinitrc .xinitrc
+arch-chroot -u "$USERNAME" /mnt ln -s ~/.config/x11/Xresources .Xresources
+arch-chroot -u "$USERNAME" /mnt ln -s ~/.config/zsh/zprofile .zprofile
+arch-chroot -u "$USERNAME" /mnt ln -s ~/.config/zsh/zshrc .zshrc
 
 echo "${green}-- Installation Completed, Restart to use your system. --${reset}"
 exit
