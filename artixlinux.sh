a076e8551988e5052ad428e966c17f6cdf1c94d5
Nipun Ravisara <34549795+nipunravisara@users.noreply.github.com>
2022-01-07 22:44:46 +0530

    Updated script

--- artixlinux.sh
+++ artixlinux.sh
@@ -11,44 +11,40 @@ DEVICE=/dev/nvme0n1
 
 BOOT_DEVICE="${DEVICE}p4"
 ROOT_DEVICE="${DEVICE}p5"
+HOME_DEVICE="${DEVICE}p6"
 
 echo "${magenta}Starting arch installation...${reset}"
-
-# change pacman parallel download to 5
-echo "${green}-- Set pacman parallel download.${reset}"
-sed '/#ParallelDownloads = 5/c\ParallelDownloads = 5' -i /etc/pacman.conf
 
 # setup keyboard layout
 echo "${green}-- Setting up keyboard layout.${reset}"
 loadkeys us
 
-# set system clock
-echo "${green}-- Set system clock.${reset}"
-timedatectl set-ntp true
-
-# format EFI partitions
-echo "${green}-- Formatting partitions.${reset}"
-lsblk
-
 # format efi partition.
 echo "${green}-- Formatting boot partitions.${reset}"
-mkfs.fat -F32 "$BOOT_DEVICE"
+mkfs.fat -F 32 "$BOOT_DEVICE"
+fatlabel "$BOOT_DEVICE" BOOT
 
 # format root partition.
 echo "${green}-- Formatting root partitions.${reset}"
-mkfs.ext4 "$ROOT_DEVICE"
+mkfs.ext4 -L ROOT "$ROOT_DEVICE"
+
+# format home partition.
+echo "${green}-- Formatting home partitions.${reset}"
+mkfs.ext4 -L HOME "$HOME_DEVICE"
 
 # mounting partitons
 echo "${green}-- Mounting partitons.${reset}"
-mount "$ROOT_DEVICE" /mnt
 mkdir /mnt/boot
+mkdir /mnt/home
+mount "$ROOT_DEVICE" /mnt
 mount "$BOOT_DEVICE" /mnt/boot
+mount "$BOOT_DEVICE" /mnt/home
 df
 echo && echo "Partitioning is completed.  Press any key to continue..."; read empty
 
 # install linux system and essentials.
 echo "${green}-- Installing linux system and essentials.${reset}"
-basestrap /mnt base linux-lts runit elogind-runit artix-archlinux-support linux-headers linux-firmware base-devel archlinux-keyring git openssh networkmanager networkmanager-runit bluez bluez-runit bluez-utils grub os-prober ntfs-3g efibootmgr zsh
+basestrap /mnt base linux-lts runit elogind-runit artix-archlinux-support linux-headers linux-firmware base-devel git networkmanager networkmanager-runit bluez bluez-runit bluez-utils grub os-prober ntfs-3g efibootmgr
 
 # generate fstab
 echo "${green}-- Generating fstab.${reset}"
@@ -119,13 +115,14 @@ echo && echo "Services are enabled. Type any key to continue."; read empty
 
 # install grub bootloader and relevent packages
 echo "${green}-- Installing grub bootloader and relevent packages.${reset}"
-grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 
 # choosing dual boot or single boot
 read -p "${yellow}Do you wish to dualboot? [y/n]${reset}" answer
 if [[ $answer = y ]] ; then
      # enable os-prober
      echo -e "\n# Disable os-prober\nGRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+     os-prober
      
      # mount windows efi
      mkdir /boot/winefi
@@ -174,19 +171,43 @@ echo && echo "Arch repos acre configured. Type any key to continue."; read empty
 
 # install packages in official repos
 echo "${green}-- Installing utility packages.${reset}"
-su "$USERNAME" -c 'yay -Sy'
-echo && echo "Yay synced. Type any key to continue."; read empty
-pacman -Qqm
-echo && echo "Installed form yay. Type any key to continue."; read empty
-su "$USERNAME" -c 'yay -S libxft-bgra'
-echo && echo "Yay synced. Type any key to continue."; read empty
+pacman -Sy
 pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop xwallpaper nodejs yarn \
 	scrot python-pywal xclip zip unzip unrar p7zip zsh rsync rofi udisks2 ueberzug pulseaudio pulseaudio-alsa pulseaudio-bluetooth \
 	pulseaudio-jack pulseaudio-bluetooth mesa xf86-video-intel vulkan-intel powertop libinput sxhkd pamixer ranger code \
 	chromium firefox neovim htop alacritty mpv sxiv zathura zathura-pdf-mupdf libnotify dunst  highlight wmctrl deepin-gtk-theme papirus-icon-theme
-yay -Sy
-pacman -S --noconfirm breezex-cursor-theme pfetch picom-git
 echo && echo "Utility packages are installed. Type any key to continue."; read empty
+
+# set stage three installer
+stage_three_path=/home/"$USERNAME"/stage-three.sh
+sed '1,/^#stage-three$/d' stage-two.sh > $stage_three_path
+chown "$USERNAME":"$USERNAME" $stage_three_path
+chmod +x $stage_three_path
+echo && echo "Script is ready. Type any key to continue."; read empty
+su -c $stage_three_path -s /bin/sh "$USERNAME"
+exit
+
+#stage-three
+
+# colors
+reset=`tput sgr0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+magenta=`tput setaf 5`
+
+# create folders
+cd $HOME
+echo "${green}-- Create folders.${reset}"
+mkdir -p ~/Documents ~/Developments ~/Pictures/Wallpapers ~/Videos
+ls -la
+echo && echo "Folders are created. Type any key to continue."; read empty
+
+# download wallpaper
+echo "${green}-- Download wallpaper.${reset}"
+curl -o ~/Pictures/Wallpapers/Nissan-skyline.jpeg https://images.hdqwalls.com/wallpapers/nissan-gtr-r34-anime-girl-5k-dn.jpg
+wal -i  ~/Pictures/Wallpapers/Nissan-skyline.jpeg
+echo && echo "Wallpaper is downloaded. Type any key to continue."; read empty
 
 # install window manager
 echo "${green}-- Select a window manager to install.${reset}"
@@ -206,10 +227,10 @@ do
             ;;
         "DWM")
             echo "${green}-- Installing $opt.${reset}"
-            git clone https://github.com/nipunravisara/dwm.git /home/$USERNAME/dwm
-	    git clone https://github.com/nipunravisara/dwmblocks.git /home/$USERNAME/dwmblocks
-	    sudo -S make clean install -C /home/$USERNAME/dwm
-	    sudo -S make clean install -C /home/$USERNAME/dwmblocks
+            git clone https://github.com/nipunravisara/dwm.git ~/dwm
+	    git clone https://github.com/nipunravisara/dwmblocks.git ~/dwmblocks
+	    sudo -S make clean install -C ~/dwm
+	    sudo -S make clean install -C ~/dwmblocks
 	    echo && echo "DWM is installed. Type any key to continue."; read empty
             break
             ;;
@@ -221,24 +242,6 @@ do
     esac
 done
 echo && echo "Window manager is installed. Type any key to continue."; read empty
-
-# set stage three installer
-stage_three_path=/home/"$USERNAME"/stage-three.sh
-sed '1,/^#stage-three$/d' stage-two.sh > $stage_three_path
-chown "$USERNAME":"$USERNAME" $stage_three_path
-chmod +x $stage_three_path
-echo && echo "Script is ready. Type any key to continue."; read empty
-su -c $stage_three_path -s /bin/sh "$USERNAME"
-exit
-
-#stage-three
-
-# colors
-reset=`tput sgr0`
-red=`tput setaf 1`
-green=`tput setaf 2`
-yellow=`tput setaf 3`
-magenta=`tput setaf 5`
 
 # install dotfiles
 echo "${green}-- Installing dotfiles.${reset}"
@@ -279,18 +282,6 @@ echo && echo "Vimplug is installed. Type any key to continue."; read empty
 echo "${green}-- Install ranger icons.${reset}"
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 echo && echo "Ranger icons are installed. Type any key to continue."; read empty
-
-# create folders
-cd $HOME
-echo "${green}-- Create folders.${reset}"
-mkdir -p ~/Documents ~/Developments ~/Pictures/Wallpapers ~/Videos
-ls -la
-echo && echo "Folders are created. Type any key to continue."; read empty
-
-# download wallpaper
-echo "${green}-- Download wallpaper.${reset}"
-curl -o ~/Pictures/Wallpapers/Nissan-skyline.jpeg https://images.hdqwalls.com/wallpapers/nissan-gtr-r34-anime-girl-5k-dn.jpg
-echo && echo "Wallpaper is downloaded. Type any key to continue."; read empty
 
 # remove unwated files
 echo "${green}-- Cleaning and linking.${reset}"
